ae3ff3f7fc78a3104176ca42c0ff66800d24d604
Gabriela Aguilar <contacto.gabriela.aguilar@gmail.com>
2024-06-14 10:19:27 -0500

    arreglos en el menu

--- app/UI/template/Header/Header.jsx
+++ app/UI/template/Header/Header.jsx
@@ -1,62 +1,68 @@
 "use client";
-import { useState } from 'react';
-import styles from './Header.module.css'
+import { useState, useEffect } from 'react';
+import styles from './Header.module.css';
 import Link from 'next/link';
 
-
-
-
 export const Header = () => {
+  const [menuMobile, setMenuMobile] = useState(false);
+  const [menuAnimate, setMenuAnimate] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
 
-
-  const [menuMobile, setMenuMobile] = useState(false)
-  const [menuAnimate, setMenuAnimate] = useState(false)
   const activeMenuMobile = () => {
-    setMenuMobile(true)
+    setMenuMobile(true);
     setTimeout(() => {
-      setMenuAnimate(true)
+      setMenuAnimate(true);
     }, 500);
-  }
-  const desactiveMenuMobile = () => {
-    setMenuMobile(false)
-    setTimeout(() => {
-      setMenuAnimate(false)
-    }, 500);
-  }
+  };
 
-  
+  const desactiveMenuMobile = () => {
+    setMenuMobile(false);
+    setTimeout(() => {
+      setMenuAnimate(false);
+    }, 500);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-    <header className={`${styles.Header}  `}>
-      <div className={styles.Header_logo}>
-        <Link href="/"> <img src='/gabrielaAg.svg' /></Link>
-      </div>
-      <div className={`${styles.Header_links} ${menuMobile ? styles.Header_linksMobile : ""} ${menuAnimate ? styles.Header_linksMobileAnimate : ""}`}>
-        <a href="/sobre-mi" className={styles.Header__a}>Sobre Mí</a>
-        <a href="/#MyPortfolio" className={styles.Header__a}>Mi Portafolio</a>
-        <button href="/#MyPortfolio" className={styles.Header__buttonLanguage} >English</button>
-        <Link href="https://portfolionext-pi.vercel.app/curriculumVitaeGabrielaAguilar.pdf" className={`${styles.Header__a} ${styles.Header__button} ` } target="_blank">Descargar Cv</Link>
-      </div>
-      {
-        menuMobile ? (
-          <div className={styles.Header__mobile} onClick={() => desactiveMenuMobile()} >
-            <span className={styles.Header__span}  > <img src="/x.svg" className={styles.Header__menuM} /></span>
-          </div>
-        ) : (
-          <div className={styles.Header__mobile} onClick={() => activeMenuMobile()} >
-            <span className={styles.Header__span}  > <img src="/menu.svg" className={styles.Header__menuM} /></span>
-          </div>
-        )
-
-
-        
-      }
-
-
-    </header>
-    
+      <header className={`${styles.Header} ${isSticky ? styles.sticky : ''}`}>
+        <div className={styles.Header_logo}>
+          <Link href="/"> <img src='/gabrielaAg.svg' /></Link>
+        </div>
+        <div className={`${styles.Header_links} ${menuMobile ? styles.Header_linksMobile : ""} ${menuAnimate ? styles.Header_linksMobileAnimate : ""}`}>
+          <a href="/sobre-mi" className={styles.Header__a}>Sobre Mí</a>
+          <a href="/#MyPortfolio" className={styles.Header__a}>Mi Portafolio</a>
+          <button href="/#MyPortfolio" className={styles.Header__buttonLanguage} >English</button>
+          <Link href="https://portfolionext-pi.vercel.app/curriculumVitaeGabrielaAguilar.pdf" className={`${styles.Header__a} ${styles.Header__button}`} target="_blank">Descargar Cv</Link>
+        </div>
+        {
+          menuMobile ? (
+            <div className={styles.Header__mobile} onClick={() => desactiveMenuMobile()} >
+              <span className={styles.Header__span}><img src="/x.svg" className={styles.Header__menuM} /></span>
+            </div>
+          ) : (
+            <div className={styles.Header__mobile} onClick={() => activeMenuMobile()} >
+              <span className={styles.Header__span}><img src="/menu.svg" className={styles.Header__menuM} /></span>
+            </div>
+          )
+        }
+      </header>
     </>
   )
 }
 
-export default Header
+export default Header;
